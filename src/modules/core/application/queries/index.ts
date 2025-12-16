cafbdf836c4aedb6d3390ffb94ee02773de11933
Willian Rodrigues <willianrdriguesz@gmail.com>
2025-12-15 21:08:05 -0300

    feat: criando endpoint busca categoria por id

--- src/modules/core/application/queries/index.ts
+++ src/modules/core/application/queries/index.ts
@@ -3,6 +3,7 @@ import { BuscarTodosLancamentosQuery } from './carteira/buscar-todos-lancamentos
 import { BuscarLancamentoPorIdQuery } from './carteira/buscar-lancamento-por-id.query';
 import { FiltrarLancamentosQuery } from './carteira/filtrar-lancamentos.query';
 import { BuscarTodasCategoriasQuery } from './categoria/buscar-todas-categorias.query';
+import { BuscarCategoriaPorIdQuery } from './categoria/buscar-categoria-por-id.query';
 
 export const CoreQueries = [
   BuscarSaldoMensalQuery,
@@ -10,4 +11,5 @@ export const CoreQueries = [
   BuscarLancamentoPorIdQuery,
   FiltrarLancamentosQuery,
   BuscarTodasCategoriasQuery,
+  BuscarCategoriaPorIdQuery,
 ];
--- src/modules/core/controllers/categoria.controller.ts
+++ src/modules/core/controllers/categoria.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { CategoriaDto } from '../application/dtos/categoria/categoria.dto';
 import { BuscarTodasCategoriasQuery } from '../application/queries/categoria/buscar-todas-categorias.query';
+import { BuscarCategoriaPorIdQuery } from '../application/queries/categoria/buscar-categoria-por-id.query';
 
 @Controller('categoria')
 export class CategoriaController {
   constructor(
     private readonly buscarTodasCategoriasQuery: BuscarTodasCategoriasQuery,
+    private readonly buscarCategoriaPorIdQuery: BuscarCategoriaPorIdQuery,
   ) {}
 
   @Get()
   async buscarTodas(): Promise<CategoriaDto[]> {
     return this.buscarTodasCategoriasQuery.execute();
+  }
+
+  @Get('/:id')
+  async buscarPorId(@Param('id') id: string): Promise<CategoriaDto> {
+    return this.buscarCategoriaPorIdQuery.execute(parseInt(id, 10));
   }
 }
